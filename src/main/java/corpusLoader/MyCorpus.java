cfea45b6cba67687f557fde57f40685d9d508ba6
Ruben <ruben@eschauzier.org>
2020-04-01 19:59:51 +0200

    optimized performance

--- src/main/java/corpusLoader/MyCorpus.java
+++ src/main/java/corpusLoader/MyCorpus.java
@@ -3,14 +3,21 @@
  */
 package corpusLoader;
 
+import java.io.BufferedWriter;
 import java.io.FileInputStream;   
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
 import java.util.Set;
 
 import com.google.gson.Gson;
@@ -18,10 +25,8 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonStreamParser;
 
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
-
-import edu.stanford.nlp.simple.Sentence;
-
 
 
 
@@ -30,18 +35,20 @@ public class MyCorpus{
 
 	private String filelocation_review;
 	private String filelocation_business;
-	private ArrayList<String> restaurants = new ArrayList<String>();
+	private String filelocation_pos;
+	private List<String> restaurants = new ArrayList<String>();
 	private Set<String> allTerms = new HashSet<String>();
 	
 	
-	public MyCorpus(String filelocation_review, String filelocation_business) {
+	public MyCorpus(String filelocation_review, String filelocation_business, String filelocation_pos) {
 		this.filelocation_review = filelocation_review;
 		this.filelocation_business = filelocation_business;
+		this.filelocation_pos = filelocation_pos;
 		
 	}
 	
 
-	public ArrayList<String> business_identifier() throws FileNotFoundException, UnsupportedEncodingException {
+	public List<String> business_identifier() throws FileNotFoundException, UnsupportedEncodingException {
 	   //int counter = 0;	
 	   InputStream is_b = new FileInputStream(filelocation_business);
 	   Reader r_b = new InputStreamReader(is_b, "UTF-8");
@@ -68,44 +75,92 @@ public class MyCorpus{
 		
 	
 	public void review_loader() throws FileNotFoundException, UnsupportedEncodingException {
-		MaxentTagger tagger = new MaxentTagger("C:\\Users\\Ruben\\git\\Heracles\\stanford-postagger-2018-10-16\\models\\english-bidirectional-distsim.tagger");
+		int counter = 0;
+	    String pattern ="[\\p{Punct}&&[^@',&]]";
+		Properties props = new Properties();
+	    // set the list of annotators to run
+	    props.setProperty("annotators", "tokenize,ssplit,pos,lemma");
+	    // set a property for an annotator, in this case the coref annotator is being
+	    // build pipeline
+	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+	    
+		MaxentTagger tagger = new MaxentTagger(filelocation_pos);
 		InputStream is_r = new FileInputStream(filelocation_review);
 		Reader r_r = new InputStreamReader(is_r, "UTF-8");
 		Gson gson_r = new GsonBuilder().create();
 		JsonStreamParser p = new JsonStreamParser(r_r);
 		while (p.hasNext()) {
+			counter += 1;
 			JsonElement e = p.next();
 			if (e.isJsonObject()) {
-				review review = gson_r.fromJson(e, review.class);
+				Review review = gson_r.fromJson(e, Review.class);
 				if (restaurants.contains(review.get_id())) {
-					review.get_sentences();
-					review.pos_tagger(tagger);
-					ArrayList<String> terms_in_review = review.get_adj_verb_noun();
-					allTerms.addAll(terms_in_review);
-					System.out.println(allTerms.size());
+					HashSet<String> review_terms = review.stanford_pipeline_tagger(pipeline, pattern);
+					allTerms.addAll(review_terms);
+					System.out.println("size:" + allTerms.size() + "reviews processed: " + counter);
+
+					// review.get_sentences();
+					// review.pos_tagger(tagger);
+					// ArrayList<String> terms_in_review = review.get_adj_verb_noun();
+					// allTerms.addAll(terms_in_review);
+					// System.out.println(allTerms.size());
 					// System.out.println(sentences_list);
 					// System.out.println(verbs);					
 				}
-				else
-				{
-					System.out.println("A non restaurant found!");
+				}
+			}
+		}
+	
+	
+	public void review_loader_own() throws FileNotFoundException, UnsupportedEncodingException {
+		String[] needed_tags = {"VB","VBD", "VBG", "VBN","VBP","VBZ","VH","VHD","VHG","VHN","VHP","VHZ","VV","VVD","VVG","VVN","VVP","VVZ","JJ","JJR","JJS","NN","NNS","NP","NPS" };
+		ArrayList<String> needed_tags_l = new ArrayList<String>(Arrays.asList(needed_tags));
+		int counter = 0;
+		MaxentTagger tagger = new MaxentTagger(filelocation_pos);
+		InputStream is_r = new FileInputStream(filelocation_review);
+		Reader r_r = new InputStreamReader(is_r, "UTF-8");
+		Gson gson_r = new GsonBuilder().create();
+		JsonStreamParser p = new JsonStreamParser(r_r);
+		while (p.hasNext()) {
+			counter += 1;
+			JsonElement e = p.next();
+			if (e.isJsonObject()) {
+				Review review = gson_r.fromJson(e, Review.class);
+				if (restaurants.contains(review.get_id())) {
+					review.get_pos_tags(tagger);
+					HashSet<String> terms_in_review = review.get_adj_noun_verb_new(needed_tags_l);
+					allTerms.addAll(terms_in_review);
+					System.out.println("size:" + allTerms.size() + "reviews processed: " + counter);
+					// System.out.println(verbs);					
 				}
 				}
 
 			}
 		}
-		
+	
+	public void write_to_file() throws IOException {
+		BufferedWriter out = new BufferedWriter(new FileWriter("E:\\OutputTerms\\Output_stanford.txt"));
+		Iterator<String> it = allTerms.iterator(); // why capital "M"?
+		while(it.hasNext()) {
+		    out.write(it.next());
+		    out.newLine();
+		}
+		out.close();
+
+	}
 		
 	
-	public static void main(String args[]) throws FileNotFoundException, UnsupportedEncodingException {
-		MyCorpus yelp_dataset = new MyCorpus("E:\\review.json", "E:\\business.json");
-		ArrayList<String> restaurants = yelp_dataset.business_identifier();
+	public static void main(String args[]) throws IOException {
+		// WHEN YOU RUN THE FILE CHANGE THE 3 FILELOCATIONS OF THE MYCORPUS CLASS!
+		MyCorpus yelp_dataset = new MyCorpus("E:\\review.json", "E:\\business.json", "C:\\Users\\Ruben\\git\\Heracles\\stanford-postagger-2018-10-16\\models\\english-left3words-distsim.tagger");
+		List<String> restaurants = yelp_dataset.business_identifier();
 		// for(int i =0; i < rest.size(); i++) {
 		//	  if (rest.lastIndexOf(rest.get(i)) != i)  {
 		// 	     System.out.println(rest.get(i)+" is duplicated");
 	    //      }
 		//   }
 		yelp_dataset.review_loader();
+		yelp_dataset.write_to_file();
 		System.out.println(yelp_dataset.allTerms);
 			}
 	
--- src/main/java/corpusLoader/review.java
+++ src/main/java/corpusLoader/review.java
@@ -1,60 +1,92 @@
 package corpusLoader;
 
-import java.text.BreakIterator;
+import java.io.Reader;
+import java.io.StringReader;
+import java.text.BreakIterator; 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Locale;
+import java.util.Properties;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.TaggedWord;
+import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.CoreSentence;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.simple.Sentence;
+import edu.stanford.nlp.simple.Token;
 import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
-public class review {
+
+// https://codereview.stackexchange.com/questions/239716/extracting-all-nouns-verbs-and-adjectives-from-a-large-text-dataset
+
+
+public class Review {
 	private  String text;
 	private String business_id;
 	private int stars;
 	private ArrayList<String> listOfSentences = new ArrayList<String>();
 	private ArrayList<String> pos_tags = new ArrayList<String>();
-	private ArrayList<String> all_terms = new ArrayList<String>();
+	private HashSet<String> all_terms = new HashSet<String>();
+	private ArrayList<TaggedWord> tagged = new ArrayList<TaggedWord>();
 	// private ArrayList<ArrayList<String>> listOfSentences = new ArrayList<ArrayList<String>>();
 	
-	public review() {
+	public Review() {
 	}
 	
-	public ArrayList<String> get_sentences(){
-		BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
-		iterator.setText(text);
-		int start = iterator.first();
-		for (int end = iterator.next();
-		    end != BreakIterator.DONE;
-		    start = end, end = iterator.next()) {
-		  listOfSentences.add(text.substring(start,end).toLowerCase().replace("\r\n", " ").replace("\n", " ").replaceAll("\\p{Punct}", ""));
+
+	
+	public void get_pos_tags(MaxentTagger tagger) {
+		Reader reader = new StringReader(text);
+		DocumentPreprocessor dp = new DocumentPreprocessor(reader);
+	      for (List<HasWord> sentence : dp) {
+	    	  tagged.addAll(tagger.tagSentence(sentence));
+	        }   
+	}
+	
+	
+	public HashSet<String >get_adj_noun_verb_new(ArrayList<String> needed_tags){
+		for(int i = 0; i < tagged.size(); i ++) {
+			if (needed_tags.contains(tagged.get(i).tag())) {
+				all_terms.add(tagged.get(i).word().toLowerCase());
+				
+			}
 		}
-		
-		return listOfSentences;
-	}
-	
-	public ArrayList<String> pos_tagger(MaxentTagger tagger){
-		// MaxentTagger tagger = new MaxentTagger("C:\\Users\\Ruben\\git\\Heracles\\stanford-postagger-2018-10-16\\models\\english-bidirectional-distsim.tagger");
-		for (int i = 0; i <listOfSentences.size(); i ++) {
-			String tagged = tagger.tagString(listOfSentences.get(i));
-			pos_tags.add(tagged);
-		}
-		return pos_tags;	
-	}
-	
-	public ArrayList<String> get_adj_verb_noun() {
-		for( int j = 0; j<pos_tags.size(); j++) {
-				Pattern regex = Pattern.compile("\\b(\\w+(?:_VB|_VBD|_VBG|_VBN|_VBP|_VBZ|_VH|_VHD|_VHG|_VHN|_VHP|_VHZ|_VV|_VVD|_VVG|_VVN|_VVP|_VVZ|_JJ|_JJR|_JJS|_NN|_NNS|_NP|_NPS|_))\\b");
-			    Matcher regexMatcher = regex.matcher(pos_tags.get(j));
-			    while (regexMatcher.find()) {
-			        all_terms.add(regexMatcher.group(1).split("_")[0]);
-			        // match end: regexMatcher.end()	
-			    }
-		    } 
-		// System.out.println(all_terms);
 		return all_terms;
 	}
 	
+	
+	public HashSet<String> stanford_pipeline_tagger(StanfordCoreNLP pipeline, String pattern) {
+    CoreDocument doc = new CoreDocument(text);
+    pipeline.annotate(doc);
+    for(int f = 0; f <doc.sentences().size(); f++) {
+    	for (int d = 0; d < doc.sentences().get(f).tokens().size(); d++) {
+    		String tag = doc.sentences().get(f).posTags().get(d);
+    		CoreLabel word = doc.sentences().get(f).tokens().get(d);
+        	if (tag.contains("VB") == true|| tag.contains("JJ") == true || tag.contains("NN") == true){
+        	    // Create a Pattern object
+        	    Pattern r = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+        	    // Now create matcher object.
+        	    Matcher m = r.matcher(word.lemma());
+        	    if (m.find() || word.originalText() == "") {
+        	    	
+        	    } else {
+        	       all_terms.add(word.lemma().toLowerCase());
+        	    }
+        	}
+    		
+    	}
+    }
+    return all_terms;
+
+    
+	}
 	public String get_text() {
 		return this.text;
 	}
@@ -66,4 +98,38 @@ public class review {
 	public int get_stars() {
 		return this.stars;
 	}
+	
+	
+	/*
+	 * 
+	 * public ArrayList<String> get_sentences(){ BreakIterator iterator =
+	 * BreakIterator.getSentenceInstance(Locale.US); iterator.setText(text); int
+	 * start = iterator.first(); for (int end = iterator.next(); end !=
+	 * BreakIterator.DONE; start = end, end = iterator.next()) {
+	 * listOfSentences.add(text.substring(start,end).toLowerCase().replace("\r\n",
+	 * " ").replace("\n", " ").replaceAll("\\p{Punct}", "")); } return
+	 * listOfSentences; }
+	 */
+	
+	
+	/*
+	 * public ArrayList<String> pos_tagger(MaxentTagger tagger){ // MaxentTagger
+	 * tagger = new MaxentTagger(
+	 * "C:\\Users\\Ruben\\git\\Heracles\\stanford-postagger-2018-10-16\\models\\english-bidirectional-distsim.tagger"
+	 * ); for (int i = 0; i <listOfSentences.size(); i ++) { String tagged =
+	 * tagger.tagString(listOfSentences.get(i)); pos_tags.add(tagged); } return
+	 * pos_tags; }
+	 */
+	
+	/*
+	 * public HashSet<String> get_adj_verb_noun() { for( int j = 0;
+	 * j<pos_tags.size(); j++) { Pattern regex = Pattern.compile(
+	 * "\\b(\\w+(?:_VB|_VBD|_VBG|_VBN|_VBP|_VBZ|_VH|_VHD|_VHG|_VHN|_VHP|_VHZ|_VV|_VVD|_VVG|_VVN|_VVP|_VVZ|_JJ|_JJR|_JJS|_NN|_NNS|_NP|_NPS|_))\\b"
+	 * ); Matcher regexMatcher = regex.matcher(pos_tags.get(j)); while
+	 * (regexMatcher.find()) { String lemma = new
+	 * Sentence(regexMatcher.group(1).split("_")[0]).lemma(0); //
+	 * System.out.println(regexMatcher.group(1).split("_")[0]);
+	 * all_terms.add(lemma); // match end: regexMatcher.end() } } return all_terms;
+	 * }
+	 */
 }
