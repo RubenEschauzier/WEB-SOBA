4bc677f844490f6895b6897e1df84bea675f4096
Fenna ten Haaf <61841371+FennatenHaaf@users.noreply.github.com>
2020-04-13 17:48:31 +0200

    classes we need for the skeletal ontology, will be changed

--- src/main/java/seminarOntologyBuilder/MainOntologyBuilder.java
+++ src/main/java/seminarOntologyBuilder/MainOntologyBuilder.java
@@ -1,5 +1,17 @@
 package seminarOntologyBuilder;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+
+//import org.json.JSONException;
+
+//import edu.eur.absa.Framework;
+//import edu.eur.absa.model.exceptions.IllegalSpanException;
+//import edu.eur.absa.seminarhelper.SeminarOntology;
+
+
+
 public class MainOntologyBuilder {
 
 }
